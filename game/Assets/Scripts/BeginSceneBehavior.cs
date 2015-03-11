72f13db8613f82b404f17ef58335a2ca2d17bc3a
CheersChao <zhch040200@163.com>
2015-03-11 13:23:00 +0800

    UI

---
+++ game/Assets/Scripts/BeginSceneBehavior.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BeginSceneBehavior : MonoBehaviour {
+
+
+    public static bool hasvolume;
+    public AudioSource audio;
+
+	// Use this for initialization
+	void Start () {
+        hasvolume = true;
+        audio = gameObject.GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void LoadMissionSelect() {
+        Application.LoadLevel("MissionSelection");
+    
+    }
+    public void LoadMission1() {
+        Application.LoadLevel("mission1-1");
+    }
+    public void ForbbidenVolume() {
+        if (hasvolume)
+        {
+            hasvolume = false;
+            audio.volume = 0;
+        }
+        else {
+            hasvolume = true;
+            audio.volume = 1;
+        }
+
+    }
+}
---
+++ game/Assets/Scripts/SettingGame.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SettingGame : MonoBehaviour {
+
+
+    public TweenPosition Tposition;
+    public static bool onthebackground;
+	// Use this for initialization
+	void Start () {
+        onthebackground = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void getmenu() {
+        Tposition.enabled = true;
+        if (onthebackground)
+        {
+            Tposition.PlayReverse();
+            onthebackground = false;
+            Time.timeScale = 1.0f;
+        }
+        else
+        {
+            Time.timeScale = 0.0f;
+            Tposition.PlayForward();
+            onthebackground = true;
+
+        }
+    }
+    public void backtoMissionSelection() {
+        Application.LoadLevel("MissionSelection");
+    }
+}
